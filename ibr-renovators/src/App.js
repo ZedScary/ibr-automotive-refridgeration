3e7f70308609547aaf02b1d5ef4153b32d26dad2
ZedScary <shaldon60@gmail.com>
2024-10-28 15:09:02 +0200

    Changes to Contact us section

--- ibr-renovators/src/App.js
+++ ibr-renovators/src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>IBR RENOVATORS</h1>
+        <h1 onClick={handleAboutUsClick}>IBR Automotive Refrigeration</h1>
         <div className="Header-buttons">
           {!showContact ? (
           <a href="#" className='Contact-us' onClick={handleContactClick}>
@@ -39,35 +39,43 @@ function App() {
       </header>
 
       {!showContact ? (
-        <div className="About-us">
-          <h1>About us</h1>
-          <p>
-            Welcome to IBR Renovators! Located just outside Klerksdorp, we specialize in automotive air-conditioning repairs and installations, providing top-quality compressors and accessories. With extensive experience, our team delivers reliable field service repairs for all makes of tractors, combines, excavators, front-end loaders, and earthmoving equipment. At IBR Renovators, we are committed to keeping your machinery and vehicles running smoothly, wherever you are.
-          </p>
-        </div>
-      ) : (
-        <div className="Contact-details">
-          <h1>Contact us</h1>
-          <p>Phone: +27 78 673 5465</p>
-          <p>Email: Louwrens.ibr@gmail.com</p>
-        </div>
-      )}
-
-      <div className='Services'>
-        <h1>Services</h1>
-        <div className='Images'>
-          <a className='Repair' href='/'>
-            <span>Repair and install of auto motive air-conditioning</span>
-          </a>
-          <a className='Supply' href='/'>
-            <span>Supply of compressors and all accesspries</span>
-          </a>
-          <a className='Field-repair' href='/'>
-            <span>Field service repair</span>
-          </a>
+        <div className='Landing-page'>
+          <div className="About-us">
+            <h1>About us</h1>
+            <p>
+              Welcome to IBR Automotive Refrigeration! Located just outside Klerksdorp, we specialize in automotive air-conditioning repairs and installations, providing top-quality compressors and accessories. With extensive experience, our team delivers reliable field service repairs for all makes of tractors, combines, excavators, front-end loaders, and earthmoving equipment. At IBR Automotive Refrigeration, we are committed to keeping your machinery and vehicles running smoothly, wherever you are.
+            </p>
+          </div>
+          <div className='Services'>
+          <h1>Services</h1>
+          <div className='Images'>
+            <a className='Repair' onClick={handleContactClick}>
+              <span>Repair and install of auto motive air-conditioning</span>
+            </a>
+            <a className='Supply' onClick={handleContactClick}>
+              <span>Supply of compressors and all accessories</span>
+            </a>
+          </div>
         </div>
       </div>
-
+      ) : (
+        <div className='Contact'>
+          <div className="Contact-details">
+            <h1>Contact us</h1>
+            <p>Phone: +27 78 673 5465</p>
+            <p>Email: Louwrens.ibr@gmail.com</p>
+          </div>
+          <div className='Experience'>
+            <h1>Our Work</h1>
+            <div className='Exp-img'>
+              <div className='photo1'></div>
+              <div className='photo2'></div>
+              <div className='photo3'></div>
+              <div className='photo4'></div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
